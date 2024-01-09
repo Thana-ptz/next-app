7c0ff9ab1f8bbc9bf384dd850f67542c06f03a15
Thana-ptz <154771726+Thana-ptz@users.noreply.github.com>
2024-01-10 05:30:13 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo - THANA Business.png" alt="Netlify Logo" className={styles.logo} />
+        <img src="/logo-thana_business.svg" alt="Logo - THANA Business" className={styles.logo} />
       </footer>
     </>
   )
